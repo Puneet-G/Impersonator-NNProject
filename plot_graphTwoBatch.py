90ab2ad509757aa5ad992a4dcbf3763a1516f691
Puneet <EngineerP@users.noreply.github.com>
2020-04-19 21:38:34 -0400

    two batch loss calculations

---
+++ plot_graphTwoBatch.py
@@ -0,0 +1,115 @@
+'''
+TODO:
+- epochs (lines)
+- iteration
+- grid
+- 0.25 y-axis epochscale
+
+'''
+# importing packages
+import pandas as pd
+import numpy as np
+import re
+import os
+import matplotlib.pyplot as plt
+import argparse
+from matplotlib.pyplot import figure
+# 
+# setting default fig size
+figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
+#
+# defining arguement parser
+parser = argparse.ArgumentParser()
+parser.add_argument("-P", "--path", help="path to log file", required=True)
+args = vars(parser.parse_args())
+#
+# output path derived from current __file__ location
+output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'plots_TwoBatchLoss')
+if not os.path.exists(output_dir):
+  os.mkdir(output_dir)
+# extract experiment name and create folder
+load_path_split = args['path'].split('/')
+exp_name = load_path_split[1]
+output_path = os.path.join(output_dir, exp_name)
+if not os.path.exists(output_path):
+  os.mkdir(output_path)
+#
+column_list = ['val_type', 'epoch', 'it_n', 'it_max', 'g_rec', 'g_tsf', 'g_style', 'g_face', 'g_adv', 'g_mask', 'g_mask_smooth', 'd_real', 'd_fake', 'd_real_loss', 'd_fake_loss']
+val_cols = ['g_rec', 'g_tsf', 'g_style', 'g_face', 'g_adv', 'g_mask', 'g_mask_smooth', 'd_real', 'd_fake', 'd_real_loss', 'd_fake_loss']
+y_min , y_max, y_diff = (-2, 6, 0.5)
+def get_value(row):
+    # loss values
+    values = [float(re.search(r':(?:\+|\-)?\d+\.\d+\n', x).group(0)[1:-1]) for x in row[1:-1]]
+    val_dict = dict(zip(val_cols, values))
+    ret_dict = {
+        'val_type': re.search(r'\(\w+,', row[0]).group(0)[1:-1],
+        'epoch': int(re.search(r'epoch: \d+', row[0]).group(0)[7:]),
+    }
+    # combining
+    ret_dict.update(val_dict)
+    return ret_dict
+# reading the loss file
+data_buffer = []
+with open(args['path'],'r') as f:
+    data_buffer.append(f.readlines())
+# split into data points
+indices = [i for i, s in enumerate(data_buffer[0]) if 'epoch' in s]
+data_lists = np.array_split(data_buffer[0], indices)
+data = [i for i in data_lists if len(i)>10] 
+# extract relevant values from
+clean_data = [get_value(i) for i in data]
+# convert to dataframe
+data_df = pd.DataFrame(clean_data)
+# remove columns - not to plot
+keep_cols = val_cols
+keep_cols.remove('g_style')
+data_splits = {
+    'Validation': data_df[data_df.val_type=='V'],
+    'Training': data_df[data_df.val_type!='V']
+}
+for name, df in data_splits.items():
+    df = df.reset_index()
+    df['e_comp'] = df['epoch'].diff()
+    epoch_lines = df[df.e_comp!=0].index
+    for i in keep_cols:
+        fig, ax = plt.subplots()
+        ax.plot(df.index, df.loc[:,i], label=i, linewidth=0.75)
+        ax.legend(loc='upper right', fontsize=6)
+        # x axis
+        ax.xaxis.set_label_text('# of epochs')
+        ax.set_xticks(epoch_lines)
+        ax.set_xticklabels(df.epoch[epoch_lines], fontsize=6)
+        y_tick_index = np.arange(y_min, y_max, y_diff)
+        ax.set_ylim([min(y_tick_index), max(y_tick_index)])
+        ax.yaxis.set_label_text('Loss value')
+        ax.set_yticks(y_tick_index)
+        ax.set_title(exp_name + '-' + name + ' Loss - ' + i)
+        plt.savefig(output_path+'/' + exp_name+'_'+name+'_'+i+'_loss.png', dpi=300)
+        plt.clf()
+        plt.cla()
+        plt.close('all')
+
+for name, df in data_splits.items():
+    df = df.reset_index()
+    df['e_comp'] = df['epoch'].diff()
+    epoch_lines = df[df.e_comp!=0].index 
+    ax.xaxis.set_label_text('# of epochs')
+    ax.set_xticks(epoch_lines)  
+    ax.set_xticklabels(df.epoch[epoch_lines], fontsize=6)
+    fig, ax = plt.subplots()
+    for i in keep_cols:
+        ax.plot(df.index, df.loc[:,i], label=i, linewidth=0.75)
+    ax.legend(loc='upper right', fontsize=6)
+    # x axis 
+    ax.xaxis.set_label_text('# of epochs')
+    ax.set_xticks(epoch_lines)
+    ax.set_xticklabels(df.epoch[epoch_lines], fontsize=6)
+    y_tick_index = np.arange(y_min, y_max, y_diff)
+    ax.set_ylim([min(y_tick_index), max(y_tick_index)])
+    ax.yaxis.set_label_text('Loss value')
+    ax.set_yticks(y_tick_index)
+    ax.set_title(exp_name + ' - ' + name + ' Loss')
+    plt.savefig(output_path+'/'+exp_name+'_'+name+'_all_loss.png', dpi=300)
+    plt.clf()
+    plt.cla()
+    plt.close('all')
---
+++ scripts/train_iPER_twobatch.sh
@@ -0,0 +1,81 @@
+#! /bin/bash
+
+# basic configs
+gpu_ids=0,1     # if using multi-gpus, increasing the batch_size
+#gpu_ids=0
+
+# dataset configs
+dataset_model=iPER  # use iPER dataset
+data_dir=/home/ronak1997_gmail_com/impersonator/train_data  # need to be replaced!!!!!
+images_folder=images
+smpls_folder=smpls
+train_ids_file=train.txt
+test_ids_file=val.txt
+
+# saving configs
+checkpoints_dir=/home/ronak1997_gmail_com/impersonator/trained_models   # directory to save models, need to be replaced!!!!!
+name=exp_ReLU_base_iPER   # the directory is ${checkpoints_dir}/name, which is used to save the checkpoints.
+loss_path="trained_models/${name}/TwoBatch_loss_log2.txt"
+trained_models_path="trained_models/${name}/"
+first_gpu=${gpu_ids:0:1}
+inf_out_path="./outputs/results/runOutput/plaid_shirt/${name}"
+inf_out_zip_path="${inf_out_path}.zip"
+graph_output="./plots_TwoBatchLoss/${name}"
+
+# model configs
+model=impersonator_trainer
+gen_name=impersonator
+image_size=256
+
+# training configs
+load_path="None"
+batch_size=24
+lambda_rec=15.0
+lambda_tsf=10.0
+lambda_face=5.0
+lambda_style=0.0
+lambda_mask=1.0
+#lambda_mask=2.5
+lambda_mask_smooth=1.0
+nepochs_no_decay=15  # fixing learning rate when epoch ranges in [0, 5]
+nepochs_decay=15    # decreasing the learning rate when epoch ranges in [6, 25+5]
+lr_G=0.0004
+lr_D=0.0001
+final_lr=0.00001
+
+python trainTwoBatch.py --gpu_ids ${gpu_ids}        \
+    --data_dir  ${data_dir}                 \
+    --images_folder    ${images_folder}     \
+    --smpls_folder     ${smpls_folder}      \
+    --checkpoints_dir  ${checkpoints_dir}   \
+    --train_ids_file   ${train_ids_file}    \
+    --test_ids_file    ${test_ids_file}     \
+    --load_path        ${load_path}         \
+    --model            ${model}             \
+    --gen_name         ${gen_name}          \
+    --name             ${name}              \
+    --dataset_mode     ${dataset_model}     \
+    --image_size       ${image_size}        \
+    --batch_size       ${batch_size}        \
+    --lambda_face      ${lambda_face}       \
+    --lambda_tsf       ${lambda_tsf}        \
+    --lambda_style     ${lambda_style}      \
+    --lambda_rec       ${lambda_rec}         \
+    --lambda_mask      ${lambda_mask}       \
+    --lambda_mask_smooth  ${lambda_mask_smooth} \
+    --lr_G             ${lr_G}              \
+    --lr_D             ${lr_D}              \
+    --final_lr         ${final_lr}          \
+    --nepochs_no_decay ${nepochs_no_decay}  --nepochs_decay ${nepochs_decay}  \
+    --mask_bce     --use_vgg       --use_face   --spectral_norm     # --label_smooth   # --gradient_penalty
+
+python plot_graphTwoBatch.py --path ${loss_path}
+sudo chmod -R 777 ${graph_output}
+zip -r plots_TwoBatchLoss.zip plots_TwoBatchLoss
+sudo chmod -R 777 plots_TwoBatchLoss.zip
+
+# ./scripts/run_inf2.sh ${name} 5 30 5 ${first_gpu}
+# sudo chmod -R 777 ${inf_out_path}
+# zip -r ${inf_out_zip_path}  ${inf_out_path}
+
+# echo Inference run is complete. The results are at: $inf_out_zip_path
